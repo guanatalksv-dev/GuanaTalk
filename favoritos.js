6fbf8e63d7058ffef1cf55465ccdb5e64e59c991
Adriitch-dev <adriana.elias2027@adoc.superate.org>
2026-05-29 10:46:06 -0600

    pagina de favoritos añadida y conectada

---
+++ favoritos.js
@@ -0,0 +1,53 @@
+function obtenerFavoritos() {
+    return JSON.parse(localStorage.getItem("favoritos")) || [];
+}
+
+function guardarFavoritos(favoritos) {
+    localStorage.setItem("favoritos", JSON.stringify(favoritos));
+}
+
+function toggleFavorito(nombre, boton) {
+
+    let favoritos = obtenerFavoritos();
+
+    if (favoritos.includes(nombre)) {
+
+        favoritos = favoritos.filter(item => item !== nombre);
+
+        boton.classList.remove("active");
+
+    } else {
+
+        favoritos.push(nombre);
+
+        boton.classList.add("active");
+    }
+
+    guardarFavoritos(favoritos);
+}
+
+/* MOSTRAR FAVORITOS */
+
+function mostrarFavoritos() {
+
+    let contenedor = document.getElementById("favoritos-container");
+
+    if (!contenedor) return;
+
+    let favoritos = obtenerFavoritos();
+
+    contenedor.innerHTML = "";
+
+    favoritos.forEach(lugar => {
+
+        contenedor.innerHTML += `
+        
+        <div class="favorito-item">
+            <h2>${lugar}</h2>
+        </div>
+
+        `;
+    });
+}
+
+mostrarFavoritos();
