dd757cdd5ca98be87d8d3d421e4d278d132c589f
kathy-1466 <katherine.zelaya2027@adoc.superate.org.sv>
2026-06-17 14:49:27 -0600

    Se agrego iconos al fondo del apartado de la descripcion larga de la legend

--- legends.php
+++ legends.php
@@ -12,11 +12,13 @@ $resultado = mysqli_query($conexion, $query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Legends</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/css/bootstrap-grid.min.css">
+    <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="styles/legends.css">
     <link rel="stylesheet" href="styles/navbar.css">
 
 </head>
 <body>
+    
    <?php include("components/navbar.php"); ?>
 
     <section class="hero">
